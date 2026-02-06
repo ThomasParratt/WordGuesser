9bd8bb93c90787cc1c9851f80a2e46f616158679
ThomasParratt <thomasparratt@googlemail.com>
2026-02-06 09:32:20 +0200

    added back image

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import './App.css'
+import back from './assets/back.svg'
 
 function App() {
   // Create an array with 30 items
@@ -16,47 +17,52 @@ function App() {
 
   return (
     <section>
-      <h1 className="text-center text-5xl">WordGuesser</h1>
-      <div className="max-w-2xl mx-auto grid grid-cols-5 grid-rows-4 gap-2 p-12">
+      <div className="max-w-2xl mx-auto w-fit grid grid-cols-5 grid-rows-4 gap-1 p-12">
         {squares.map((_, index) => (
           <div
             key={index}
-            className="bg-white aspect-square flex items-center justify-center text-black text-5xl font-bold border-2 border-gray-500"
+            className="min-h-20 bg-white aspect-square flex items-center justify-center text-black text-4xl font-bold border-2 border-gray-300"
           >
             {typed[index]}
           </div>
         ))}
       </div>
-      <div className="mt-8 max-w-4xl mx-auto w-fit grid grid-cols-10 grid-rows-1 gap-2 p-1">
+      <div className="max-w-4xl mx-auto w-fit flex gap-2 p-1">
         {row1.map((_, index) => (
           <div
             key={index}
             onClick={() => handleClick(row1[index])}
-            className="min-w-20 min-h-24 bg-gray-500 cursor-pointer active:scale-95 rounded-xl flex items-center justify-center text-white text-2xl font-bold"
+            className="min-w-10 min-h-20 bg-gray-300 cursor-pointer active:scale-95 rounded-md flex items-center justify-center text-black text-2xl font-bold"
           >
             {row1[index]}
           </div>
         ))}
       </div>
-      <div className="max-w-4xl mx-auto w-fit grid grid-cols-9 grid-rows-1 gap-2 p-1">
+      <div className="max-w-4xl mx-auto w-fit flex gap-2 p-1">
         {row2.map((_, index) => (
           <div
             key={index}
             onClick={() => handleClick(row2[index])}
-            className="min-w-20 min-h-24 bg-gray-500 cursor-pointer active:scale-95 rounded-xl flex items-center justify-center text-white text-2xl font-bold"
+            className="min-w-10 min-h-20 bg-gray-300 cursor-pointer active:scale-95 rounded-md flex items-center justify-center text-black text-2xl font-bold"
           >
             {row2[index]}
           </div>
         ))}
       </div>
-      <div className="max-w-4xl mx-auto w-fit grid grid-cols-9 grid-rows-1 gap-2 p-1">
+      <div className="max-w-4xl mx-auto w-fit flex gap-2 p-1">
         {row3.map((_, index) => (
           <div
             key={index}
             onClick={() => handleClick(row3[index])}
-            className="min-w-20 min-h-24 bg-gray-500 cursor-pointer active:scale-95 rounded-xl flex items-center justify-center text-white text-2xl font-bold"
+            className={`min-h-20 bg-gray-300 cursor-pointer active:scale-95 rounded-md flex items-center justify-center text-black font-bold ${
+              index === 0 || index === 8 ? "min-w-16 text-md" : "min-w-10 text-2xl"
+            }`}
           >
-            {row3[index]}
+            {index === 8 ? (
+              <img src={back} alt="Back" className="w-7 h-7" />
+            ) : (
+              row3[index]
+            )}
           </div>
         ))}
       </div>
