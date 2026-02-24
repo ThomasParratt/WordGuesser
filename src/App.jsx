99cc571a53b838b3549a59082a329a81e4c63f8e
ThomasParratt <thomasparratt@googlemail.com>
2026-02-24 20:08:35 +0200

    now getting random word

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import { useState } from 'react'
 import back from './assets/back.svg'
+import answers from "./assets/answers.json";
+import words from "./assets/words.json";
+
+function getRandomWordArray() {
+  const randomIndex = Math.floor(Math.random() * answers.length);
+
+  return answers[randomIndex]
+    .toUpperCase()
+    .split("");
+}
+
+const answer = getRandomWordArray();
 
 function App() {
-  const answer = ['H', 'E', 'L', 'L', 'O'];
   const squares = Array.from({ length: 30 });
   const alphabet = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'BACK'];
 
