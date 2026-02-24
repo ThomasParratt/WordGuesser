2f15aeb558dc518dd4bfee136a39e7829b06b4e6
ThomasParratt <thomasparratt@googlemail.com>
2026-02-24 20:16:49 +0200

    EOD

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function getRandomWordArray() {
 }
 
 const answer = getRandomWordArray();
-
+// NOW NEED: NOT A WORD, REVEAL WORD AT END IF DON'T GET
 function App() {
   const squares = Array.from({ length: 30 });
   const alphabet = ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P', 'A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L', 'ENTER', 'Z', 'X', 'C', 'V', 'B', 'N', 'M', 'BACK'];
